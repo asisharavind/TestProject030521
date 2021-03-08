9e59b66b10909990ba98933b74be1ced62392e0a
asish <asish@192.168.0.12>
2021-03-08 13:02:36 -0800

    Commit-0308_Added a new message

--- MyFirstMavenProject_Asish/src/test/java/Pack1/HelloWorld.java
+++ MyFirstMavenProject_Asish/src/test/java/Pack1/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
 		
 		System.out.println("Hello World");
 		System.out.println("Hello Asish Aravind");
-		System.out.println("Hello Aswathy");
+		System.out.println("Hello Automation-0308");
 	}
 
 }
