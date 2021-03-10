1716f48914631821dc5e259689a1e3bf3eaa4bac
asish <asish@192.168.0.12>
2021-03-09 18:13:29 -0800

    commit2-0309

--- MyFirstMavenProject_Asish/src/test/java/Pack1/HelloWorld.java
+++ MyFirstMavenProject_Asish/src/test/java/Pack1/HelloWorld.java
@@ -1,5 +1,7 @@
 package Pack1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class HelloWorld {
@@ -11,6 +13,10 @@ public class HelloWorld {
 		System.out.println("Hello Asish Aravind");
 		System.out.println("Hello Automation-0308");
 		System.out.println("Hello Automation-0309");
+		
+		//driver.get(System.getProperty("url"));
+		WebDriver driver = new ChromeDriver();
+		driver.get(System.getProperty("url"));
 	}
 
 }
