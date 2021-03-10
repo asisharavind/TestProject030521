b329cf0ea8a9782ca9c05bcd726cf4313d8f05da
asish <asish@192.168.0.12>
2021-03-10 12:09:46 -0800

    1st commit-0310

--- MyFirstMavenProject_Asish/src/test/java/Pack1/HelloWorld.java
+++ MyFirstMavenProject_Asish/src/test/java/Pack1/HelloWorld.java
@@ -2,6 +2,7 @@ package Pack1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class HelloWorld {
@@ -13,12 +14,21 @@ public class HelloWorld {
 		System.out.println("Hello Asish Aravind");
 		System.out.println("Hello Automation-0308");
 		System.out.println("Hello Automation-0309");
+		WebDriver driver = null;
+		if(System.getProperty("browser").equalsIgnoreCase("chrome")) {
+			
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\asish\\git\\TestProject030521\\MyFirstMavenProject_Asish\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.get(System.getProperty("url"));
+		}
+
 		
-		//driver.get(System.getProperty("url"));
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\asish\\git\\TestProject030521\\MyFirstMavenProject_Asish\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get(System.getProperty("url"));
-		System.out.println("Test Complete-Asish Aravind");
+		
+		//System.setProperty("webdriver.gecko.driver", "C:\\Users\\asish\\git\\TestProject030521\\MyFirstMavenProject_Asish\\geckodriver.exe");
+		//WebDriver driver = new FirefoxDriver();
+		driver.get("https://google.com");
+			
+		System.out.println("Test Complete-Asish Aravind-0310");
 	}
 
 }
